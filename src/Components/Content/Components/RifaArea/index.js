ccdc604c0b4124b1ba5d56488b76b87a7da770c4
Mateus Degobi <kim3dis@gmail.com>
2022-01-26 17:47:34 -0300

    Adicionando o elemento e iterando-o

--- src/Components/Content/Components/RifaArea/index.js
+++ src/Components/Content/Components/RifaArea/index.js
@@ -1,18 +1,47 @@
 import React, {useEffect, useState} from "react";
 import IconTire from '../../../../assets/image/tire.png';
 
-
-
+import * as S from './style';
 
 export default function RifaArea() {
-  const [itemsArray, setItemsArray] = useState([]);
+  const [numbers, setNumbers] = useState([]);
+  const solds = [
+    {
+      number: 10,
+    },
+    {
+      number: 2,
+    },
+    {
+      number: 5,
+    },
+    {
+      number: 1,
+    },
+    {
+      number: 4,
+    },
+  ];
 
   useEffect(()=> {
-    
+    let numbersMutable = [];
+    for (let number = 0; number <= 600; number++) {
+      numbersMutable.push({
+        number,
+        isAvailable: true,
+      });
+    }
+    setNumbers(numbersMutable);
   }, []);
 
   function HandlerItems() {
-    return <img src={IconTire} alt="" />;
+    if(numbers.length > 1) {
+      return numbers.map(number => (
+        <S.TireIcons src={IconTire} alt="" />
+      ));
+    } else {
+      return <img src={IconTire} alt="" />;
+    }
   }
 
 
