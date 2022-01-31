c1adeab7d60abfbeb4c399739a46bffebf234be7
mateusdegobi <kim3dis@gmail.com>
2022-01-31 06:50:12 -0300

    Adição do modal de compra

--- src/Components/Content/index.js
+++ src/Components/Content/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import RifaArea from './Components/RifaArea';
 import ModalM from './Components/Modals/Modal_M';
 
 export default function Content() {
+  const [isOpened, setIsOpened] = useState(false);
+  const [choosedNumber, setChoosedNumbed] = useState('0');
+
   return (
     <nav>
-      <ModalM />
-      <RifaArea />
+      <ModalM 
+        isOpened_modal={isOpened} 
+        setIsOpened_modal={setIsOpened} 
+        choosedNumber={choosedNumber} 
+      />
+
+      <RifaArea setIsOpened_modal={setIsOpened} setChoosedNumbed={setChoosedNumbed} />
     </nav>
   );
 }
