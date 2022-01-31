52af9bd9706d17b0e1abc4a673a1abeae09a4b0d
mateusdegobi <kim3dis@gmail.com>
2022-01-31 06:48:19 -0300

    Estilizando o Modal, funcionalidades e cenas

--- src/Components/Content/Components/Modals/Modal_M/index.js
+++ src/Components/Content/Components/Modals/Modal_M/index.js
@@ -1,14 +1,50 @@
 import React, {useState} from "react";
 
-import {Modal, ModalContent} from "./style";
+import * as S from "./style";
 
-export default function ModalM() {
-  const [isOpened, setIsOpened] = useState(false);
+export default function ModalM({isOpened_modal, setIsOpened_modal, choosedNumber}) {
+  const [scene1, setScene1] = useState(true);
+  const [scene2, setScene2] = useState(false);
+
+  function nextScene_continue() {
+    setScene1(false);
+    setScene2(true);
+  }
+
+  function resetConfig() {
+    setScene1(true);
+    setScene2(false);
+    setIsOpened_modal(false)
+  }
+
   return (
-    <Modal size={[300, 800]} onClick={() => setIsOpened(true)} isOpened={isOpened}>
-      <ModalContent>
-        <p>dd</p>
-      </ModalContent>
-    </Modal>
+    <S.Modal size={[300, 800]} isOpened={isOpened_modal}>
+      <S.ModalBody>
+        <S.ModalHeader>
+          <S.CloseButton onClick={resetConfig} />
+        </S.ModalHeader>
+
+      <S.Scene1 isOn={scene1}>
+        <S.ModalContent>
+          <S.Title>Agora reserve seu número</S.Title>
+          <p>Você escolheu o número <b>{choosedNumber}</b></p>
+          <p>Sinta-se a vontade para escolhe mais.</p>
+
+          <S.ButtonsArea>
+            <S.ButtonContinue onClick={nextScene_continue} />
+            <S.ButtonChooseMore />
+          </S.ButtonsArea>
+        </S.ModalContent>
+      </S.Scene1>
+
+      <S.Scene2 isOn={scene2}>
+        <S.ModalContent>
+          <S.Title>Quando ganhar, entraremos em contato com você!!</S.Title>
+          <p>Para isso, preencha as informações abaixo e verifique, pois não há como alterar depois.</p>
+        </S.ModalContent>
+      </S.Scene2>
+
+      </S.ModalBody>
+    </S.Modal>
   )
 }
--- src/Components/Content/Components/Modals/Modal_M/style.js
+++ src/Components/Content/Components/Modals/Modal_M/style.js
@@ -1,5 +1,13 @@
 import styled from 'styled-components/';
 
+export const Scene1 = styled.div`
+  display: ${props => props.isOn ? 'block' : 'none'};
+`;
+
+export const Scene2 = styled.div`
+  display: ${props => props.isOn ? 'block' : 'none'};
+`;
+
 export const Modal = styled.div`
   display: ${props => props.isOpened ? 'block' : 'none'};
   position: fixed;
@@ -14,10 +22,72 @@ export const Modal = styled.div`
   background-color: rgba(0,0,0,0.4);
 `;
 
-export const ModalContent = styled.div`
-  background-color: #555;
+export const ModalBody = styled.div`
+  background-color: #eee;
   margin: auto;
-  padding: 20px;
   border: 1px solid #888;
   width: 80%;
+`;
+
+export const ModalHeader = styled.div`
+  width: 100%;
+  background-color: #fff;
+  display: flex;
+  justify-content: right;
+`;
+
+export const CloseButton = styled.div`
+  & {
+    font-size: 30px;
+    padding: 5px 15px;
+    cursor: pointer;
+  }
+  &:after {
+    content: '×'
+  }
+`;
+
+export const ModalContent = styled.div`
+
+`;
+
+export const Title = styled.h2`
+  text-align: center;
+`;
+
+// 
+// Buttons config
+export const ButtonsArea = styled.div`
+  width: 100%;
+`;
+
+export const ButtonContinue = styled.div`
+  & {
+    display: inline-block;
+    padding: 10px;
+    background-color: tomato;
+    cursor: pointer;
+    border-radius: 5px;
+    color: #ffffffdd;
+    font-weight: 500;
+  }
+  &:after {
+    content: 'Quero só esse número';
+  }
+`;
+
+export const ButtonChooseMore = styled.div`
+  & {
+    display: inline-block;
+    padding: 10px;
+    background-color: green;
+    cursor: pointer;
+    margin: 10px;
+    border-radius: 5px;
+    color: #ffffffdd;
+    font-weight: 500;
+  }
+  &:after {
+    content: 'Quero escolher mais números';
+  }
 `;
